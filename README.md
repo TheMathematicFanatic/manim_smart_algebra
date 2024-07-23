da34d19ef0780d05baa5c15e0ff5ac5910e4202e
John Connell <johnconnelltutor@gmail.com>
2024-07-23 19:56:31 -0400

    Restructuring for poetry

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Description
+
+I don't know what I'm going to put here yet, but apparently it can't be blank or twine gives me a hard time.
---
+++ src/manim_smart_algebra/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '0.3.1'
+__author__ = 'John Connell - The Mathematic Fanatic'
+__description__ = 'Manim plugin which subclasses MathTex to make it much easier to animate algebra.'
+
+from .expressions import *
+#from .actions import *
+
+"""
+.expressions contains the main mobject classes of the entire plugin
+.actions contains the animation classes, not currently imported because they are not ready yet
+.utils not imported because they are intended for internal use only
+.nicknames not imported, they are intended to be optionally imported with * if desired.
+
+
+from manim import *
+import manim_smart_algebra as msa
+from manim_smart_algebra.nicknames import * #optional
+
+or something like that depending on preferences. I personally will do
+
+from manim import *
+from manim_smart_algebra import *
+from manim_smart_algebra.nicknames import *
+"""
