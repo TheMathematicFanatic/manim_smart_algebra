807cf8db7a48d4ed5ed947fae7bfe24278512b4e
John Connell <johnconnelltutor@gmail.com>
2024-10-10 12:30:12 -0400

    First attempt at AlgebraicAction get_addressmap

--- src/manim_smart_algebra/actions.py
+++ src/manim_smart_algebra/actions.py
@@ -210,6 +210,29 @@ class AlgebraicAction(SmartAction):
     def get_output_expression(self, input_expression=None):
         var_dict = match_expressions(self.template1, input_expression)
         return self.template2.substitute(var_dict)
+    
+    @preaddressmap
+    def get_addressmap(self):
+        addressmap = []
+        def get_var_ad_dict(template):
+            template_leaves = {
+                template.get_subex(ad)
+                for ad in self.input_expression.get_all_leaf_addresses()
+                }
+            variables = [var for var in template_leaves if isinstance(var, SmartVariable)]
+            return {var: template.get_addresses_of_subex(var) for var in variables}
+        self.template1_address_dict = get_var_ad_dict(self.template1)
+        self.template2_address_dict = get_var_ad_dict(self.template2)
+        variables = self.template1_address_dict.keys() | self.template2_address_dict.keys()
+        for var in variables:
+            kwargs = self.var_kwarg_dict.get(var, {})
+            if len(self.template1_address_dict[var]) == 1:
+                addressmap += [[self.template1_address_dict[var][0], t2ad, kwargs] for t2ad in self.template2_address_dict[var]]
+            elif len(self.template2_address_dict[var]) == 1:
+                addressmap += [[t1ad, self.template2_address_dict[var][0], kwargs] for t1ad in self.template1_address_dict[var]]
+            else:
+                raise ValueError("I don't know what to do when a variable appears more than once on both sides. Please set addressmap manually.")
+        return addressmap
 
 
 
