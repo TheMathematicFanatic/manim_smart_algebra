a3c98cf884a57d25314abecf156c55f4a9315d47
John Connell <johnconnelltutor@gmail.com>
2025-03-14 00:59:59 -0400

    Refactored TransformByGlyphMap! Pleased with it :)

--- src/manim_smart_algebra/actions/animations.py
+++ src/manim_smart_algebra/actions/animations.py
@@ -4,6 +4,9 @@ from manimlib import *
 def ir(a,b): #inclusive_range
     return list(range(a,b+1))
 
+def VG(mob, index_list):
+    return VGroup(*[mob[i] for i in index_list])
+
 class Wait(FadeOut):
     def __init__(self, wait_time, **kwargs):
         self.mobject = VMobject()
@@ -37,6 +40,14 @@ class TransformByGlyphMap(AnimationGroup):
 
         self.mobA = mobA
         self.mobB = mobB
+        self.default_introducer = default_introducer
+        self.default_remover = default_remover
+        self.introduce_individually=introduce_individually
+        self.remove_individually=remove_individually
+        self.shift_fades=shift_fades
+        self.auto_resolve = auto_resolve
+        self.show_indices = show_indices or len(glyph_map)==0
+        self.printing = printing
 
         A = self.mobA.copy() if from_copy else mobA
         for i in mobA_submobject_index:
@@ -44,102 +55,122 @@ class TransformByGlyphMap(AnimationGroup):
         B = self.mobB
         for i in mobB_submobject_index:
             B = B[i]
-        animations = []
-        mentioned_from_indices = []
-        mentioned_to_indices = []
-
-        def VG(mob, index_list):
-            return VGroup(*[mob[i] for i in index_list])
-        
-        if len(glyph_map)==0: show_indices=True
+        self.animations = []
+        self.mentioned_from_indices = []
+        self.mentioned_to_indices = []
 
         for entry in glyph_map:
-            if printing:
-                print("Glyph map entry: ", entry)
-            assert len(entry) in [2, 3], "Invalid glyph_map entry: " + str(entry)
-            entry_kwargs = {} if len(entry) == 2 else entry[2]
+            self.process_entry(A, B, entry)
+        
+        self.check_indices(A, B)
+        
+        if self.show_indices:
+            self.show_indices_animations(A, B, index_label_height, A_index_labels_color, B_index_labels_color)
+            return
 
-            if not entry[0] and not entry[1]:
-                print("Empty glyph_map entry: " + str(entry))
-                show_indices = True
-            elif not entry[0] or (isinstance(entry[0], type) and issubclass(entry[0], Animation)):
-                Introducer = entry[0] if entry[0] else default_introducer
-                introduced_mobs = [B[i] for i in entry[1]] if introduce_individually else [VG(B,entry[1])]
-                for mob in introduced_mobs:
-                    animations.append(Introducer(
-                        mob,
-                        **{
-                            **kwargs,
-                            **({"shift":B.get_center() - A.get_center()} if shift_fades else {}),
-                            **{k:v for k,v in entry_kwargs.items() if k != 'delay'}
-                        }
-                        ))
-                    if "delay" in entry_kwargs:
-                        animations[-1] = Succession(Wait(entry_kwargs["delay"]), animations[-1])
-                mentioned_to_indices += entry[1]
-            elif not entry[1] or (isinstance(entry[1], type) and issubclass(entry[1], Animation)):
-                Remover = entry[1] if entry[1] else default_remover
-                removed_mobs = [A[i] for i in entry[0]] if remove_individually else [VG(A,entry[0])]
-                for mob in removed_mobs:
-                    animations.append(Remover(
-                        mob,
-                        **{
-                            **kwargs,
-                            **({"shift":B.get_center() - A.get_center()} if shift_fades else {}),
-                            **{k:v for k,v in entry_kwargs.items() if k != 'delay'}
-                        }
-                        ))
-                    if "delay" in entry_kwargs and entry_kwargs["delay"] != 0:
-                        animations[-1] = Succession(Wait(entry_kwargs["delay"]), animations[-1])
-                mentioned_from_indices += entry[0]
-            elif len(entry[0]) > 0 and len(entry[1]) > 0:
-                animations.append(ReplacementTransform(
-                    VGroup(*[A[i].copy() if i in mentioned_from_indices else A[i] for i in entry[0]]),
-                    VG(B,entry[1]),
-                    **{
-                        **kwargs,
-                        **{k:v for k,v in entry_kwargs.items() if k != 'delay'}
-                        }
-                    ))
-                if "delay" in entry_kwargs:
-                    animations[-1] = Succession(Wait(entry_kwargs["delay"]), animations[-1])
-                mentioned_from_indices += entry[0]
-                mentioned_to_indices += entry[1]
-            else:
-                raise ValueError("Invalid glyph_map entry: " + str(entry))
-        
-        
-        remaining_from_indices = [i for i in range(len(A)) if i not in mentioned_from_indices]
-        remaining_to_indices = [i for i in range(len(B)) if i not in mentioned_to_indices]
-        if printing:
-            print("All mentioned from indices: ", mentioned_from_indices)
-            print("All mentioned to indices: ", mentioned_to_indices)
-            print(f"All remaining from indices (length {len(remaining_from_indices)}): ", remaining_from_indices)
-            print(f"All remaining to indices (length {len(remaining_to_indices)}):", remaining_to_indices)
-        
-        if not len(remaining_from_indices) == len(remaining_to_indices) and not auto_resolve:
-            print("Error: lengths of unmentioned indices do not match.")
-            show_indices = True
-        
-        if show_indices:
-            print("Showing indices...")
-            super().__init__(
-                ShowCreation(index_labels(A, label_height=index_label_height).set_color(A_index_labels_color).set_z_index(10).set_stroke(color=BLACK, width=3)),
-                FadeIn(B.next_to(A, DOWN), shift=DOWN),
-                ShowCreation(index_labels(B, label_height=index_label_height).set_color(B_index_labels_color).set_z_index(10).set_stroke(color=BLACK, width=3)),
-                Wait(5),
-                lag_ratio=0.5
-            )
+        if auto_resolve:
+            self.process_auto_resolve(A, B, auto_resolve_delay)
         else:
-            if auto_resolve:
-                for j in remaining_to_indices:
-                    animations.append(Succession(Wait(auto_resolve_delay), default_introducer(B[j])))
-                for i in remaining_from_indices:
-                    animations.append(Succession(Wait(auto_resolve_delay), default_remover(A[i])))
+            self.process_remaining(A, B)
+
+        super().__init__(*self.animations, **kwargs)
+
+    def process_entry(self, A, B, entry):
+        if self.printing:
+            print("Glyph map entry: ", entry)
+        assert len(entry) in [2, 3], "Invalid glyph_map entry: " + str(entry)
+        if len(entry) == 2:
+            entry = (*entry, {})
+        self.interpret_delay(entry[2])
+        if not entry[0] and not entry[1]:
+            self.process_empty_entry()
+        elif not entry[0] or (isinstance(entry[0], type) and issubclass(entry[0], Animation)):
+            self.process_introducer_entry(A, B, entry)
+        elif not entry[1] or (isinstance(entry[1], type) and issubclass(entry[1], Animation)):
+            self.process_remover_entry(A, B, entry)
+        elif len(entry[0]) > 0 and len(entry[1]) > 0:
+            self.process_double_entry(A, B, entry)
+        else:
+            raise ValueError("Invalid glyph_map entry: " + str(entry))
+
+    def process_empty_entry(self):
+        if self.printing:
+            print("Empty glyph_map entry.")
+        self.show_indices = True
+
+    def process_introducer_entry(self, A, B, entry):
+        Introducer = entry[0] if entry[0] else self.default_introducer
+        if Introducer == FadeIn and self.shift_fades:
+            entry[2]["shift"] = B.get_center() - A.get_center()
+        introduced_mobs = [B[i] for i in entry[1]] if self.introduce_individually else [VG(B,entry[1])]
+        for mob in introduced_mobs:
+            self.animations.append(Introducer(mob, **entry[2]))
+        self.mentioned_to_indices += entry[1]
+
+    def process_remover_entry(self, A, B, entry):
+        Remover = entry[1] if entry[1] else self.default_remover
+        if Remover == FadeOut and self.shift_fades:
+            entry[2]["shift"] = B.get_center() - A.get_center()
+        removed_mobs = [A[i] for i in entry[0]] if self.remove_individually else [VG(A,entry[0])]
+        for mob in removed_mobs:
+            self.animations.append(Remover(mob, **entry[2]))
+        self.mentioned_from_indices += entry[0]
+
+    def process_double_entry(self, A, B, entry):
+        from_mob = VGroup(*[A[i].copy() if i in self.mentioned_from_indices else A[i] for i in entry[0]])
+        to_mob = VG(B,entry[1])
+        self.animations.append(ReplacementTransform(from_mob, to_mob, **entry[2]))
+        self.mentioned_from_indices += entry[0]
+        self.mentioned_to_indices += entry[1]
+
+    def interpret_delay(self, dict):
+        delay = dict.pop("delay", 0)
+        if delay == 0:
+            return dict
+        run_time = dict.pop("run_time", 1)
+        new_run_time = delay + run_time
+        rate_func = dict.pop("rate_func", smooth)
+        def new_rate_func(t): #https://www.desmos.com/calculator/4hphvny63n
+            a = delay / new_run_time
+            if t < a:
+                return 0
             else:
-                for i,j in zip(remaining_from_indices, remaining_to_indices):
-                    animations.append(ReplacementTransform(A[i], B[j], **kwargs))
-            super().__init__(*animations, **kwargs)
+                return rate_func((t-a)/(1-a))
+        dict["rate_func"] = new_rate_func
+        dict["run_time"] = new_run_time
+        return dict
+
+    def check_indices(self, A, B):
+        self.remaining_from_indices = [i for i in range(len(A)) if i not in self.mentioned_from_indices]
+        self.remaining_to_indices = [i for i in range(len(B)) if i not in self.mentioned_to_indices]
+        if self.printing:
+            print("All mentioned from indices: ", self.mentioned_from_indices)
+            print("All mentioned to indices: ", self.mentioned_to_indices)
+            print(f"All remaining from indices (length {len(self.remaining_from_indices)}): ", self.remaining_from_indices)
+            print(f"All remaining to indices (length {len(self.remaining_to_indices)}):", self.remaining_to_indices)
+        if not len(self.remaining_from_indices) == len(self.remaining_to_indices) and not self.auto_resolve:
+            print("Error: lengths of unmentioned indices do not match.")
+            self.show_indices = True
+
+    def show_indices_animations(self, A, B, index_label_height, A_index_labels_color, B_index_labels_color):
+        print("Showing indices...")
+        super().__init__(
+            ShowCreation(index_labels(A, label_height=index_label_height).set_color(A_index_labels_color).set_z_index(10).set_stroke(color=BLACK, width=3)),
+            FadeIn(B.next_to(A, DOWN), shift=DOWN),
+            ShowCreation(index_labels(B, label_height=index_label_height).set_color(B_index_labels_color).set_z_index(10).set_stroke(color=BLACK, width=3)),
+            Wait(5),
+            lag_ratio=0.5
+        )
+
+    def process_auto_resolve(self, A, B, auto_resolve_delay):
+        for i in self.remaining_from_indices:
+            self.process_remover_entry(A, B, ([], [i], {"delay":auto_resolve_delay}))
+        for j in self.remaining_to_indices:
+            self.process_remover_entry(A, B, ([j], [], {"delay":auto_resolve_delay}))
+
+    def process_remaining(self, A, B):
+        for i,j in zip(self.remaining_from_indices, self.remaining_to_indices):
+            self.animations.append(ReplacementTransform(A[i], B[j]))
 
     def begin(self):
         # Save and later restore mobA so that it is unharmed by the transform
